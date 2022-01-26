4317c5050f4688cc23070fc54eebe91e463f77a7
Kyle Mikolajczyk <kjmikolajczyk@wpi.edu>
2022-01-26 10:33:30 -0500

    Added more code for Updates and Action support

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -11,16 +11,40 @@ WebUSB WebUSBSerial(1 /* http:// */, "localhost:4000/controller/");
  */
 
 #define DEBUG true // Enable or disable debug operation
-#define JSON_BUFFER_SIZE 1024
+#define JSON_BUFFER_SIZE 1024 // Size of JSON file in bytes
+#define NULL_STRING "null" //String of null JSON might return
+#define START_TRANSMISSION_CHAR "#"
+#define END_TRANSMISSION_CHAR "$"
+
+/**
+ * Action types
+ */
+#define ACTION "action"
+#define ACTION_BUTTON_PRESSED "buttonPressed"
+#define ACTION_TILE_REMOVED "tileRemoved"
+#define ACTION_TILE_PLACED "tilePlaced"
+#define ACTION_SLIDER_MOVED "sliderMoved"
+#define ACTION_ID "ID"
+#define ACTION_ID_BUTTON "B"
+#define ACTION_ID_TILE "T"
+#define ACTION_ID_SLIDER "S"
+
+/**
+ * Update types
+ */
+#define UPDATE "update"
+#define UPDATE_LEDS "LEDS"
+
 
 /**
  * Arduino setup function on powerup.
  */
-void setup() {
+void setup() 
+{
   while (!Serial) {;} // Don't do anything unless Serial is active
   
   Serial.begin(115200);
-  if(DEBUG) {Serial.println("Controller Paired.");}
+  if(DEBUG) {sendToSite("Controller Paired.");}
   Serial.flush();
 }
 
@@ -29,7 +53,8 @@ void setup() {
  * @args 
  * @returns DynamicJsonDocument json
  */
-DynamicJsonDocument getJsonFromSite() {
+DynamicJsonDocument getJsonFromSite() 
+{
   String jsonRecieved = Serial.readString();
   if(DEBUG) {sendToSite(jsonRecieved);}
   
@@ -42,24 +67,68 @@ DynamicJsonDocument getJsonFromSite() {
  * Send any data to site (Adds Start/end character to indicate start/end of a transmission)
  * @args data String data to send
  */
-void sendToSite(String data) {
-  String toSend = "#" + data + "$"; // Start and end markers to help process the JSON on the controller (it recieves in chunks of data)
+void sendToSite(String data) 
+{
+  String toSend = START_TRANSMISSION_CHAR + data + END_TRANSMISSION_CHAR; // Start and end markers to help process the JSON on the controller (it recieves in chunks of data)
   char buff[JSON_BUFFER_SIZE];
   toSend.toCharArray(buff,JSON_BUFFER_SIZE);
   Serial.write(buff);
   Serial.flush();
 }
 
-void serialAvailable() {
-  if (Serial && Serial.available()) {
+/**
+ * Process update sent to controller
+ */
+void processUpdate(DynamicJsonDocument json)
+{
+  String update = json[UPDATE];
+  if(DEBUG){sendToSite(update);}
+
+  if(update.equalsIgnoreCase(NULL_STRING)) {return;}
+  else if (update.equalsIgnoreCase(UPDATE_LEDS)) 
+  {
+    //Handle LED update
+  }
+  else
+  {
+    sendToSite("Update passed not supported, " + update);
+  }
+}
+
+/**
+ * Handle the action passed
+ * @args json JSON from site
+ */
+void processAction(DynamicJsonDocument json) 
+{
+  String action = json[ACTION];
+  if(DEBUG){sendToSite(action);}
+  
+  if(action.equalsIgnoreCase(NULL_STRING)) {return;} //don't do anything if there was no action key
+  else if (action.equalsIgnoreCase(ACTION_BUTTON_PRESSED))
+  {
+    
+  }
+  else if (action.equalsIgnoreCase(ACTION_TILE_REMOVED))
+  {
+    
+  }
+  
+  //Handle cases here
+}
+
+/**
+ * 
+ */
+void serialAvailable() 
+{
+  if (Serial && Serial.available()) 
+  {
     DynamicJsonDocument json = getJsonFromSite(); // Let's get the sent JSON
     
-    String action = json["action"];
+    processAction(json);
+    
     String kyle = json["kyle"];
-
-    if(DEBUG) {
-      sendToSite(action);
-    }
     Serial.flush();
   }
 }
@@ -67,6 +136,7 @@ void serialAvailable() {
 /**
  * Arduino loop function
  */
-void loop() {
+void loop() 
+{
   serialAvailable();
 }
