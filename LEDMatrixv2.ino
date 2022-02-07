9b4e8f2a2dc310e551797712fb234cd40b43db4f
yenna428 <98187358+yenna428@users.noreply.github.com>
2022-02-06 20:48:38 -0500

    Add isosceles and drunkard's curve

--- LEDMatrixv2.ino
+++ LEDMatrixv2.ino
@@ -47,6 +47,51 @@ FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds[0], leds.Size());
     }
   }
 
+//Isoceles
+  for (int i = 0; i < MATRIX_WIDTH; i++)
+    {
+      leds(i,0) = CHSV(150, 255, 50);
+      }
+  for (int k = 1; k < 3; k++)
+    {
+      for (int j = 1; j < 7; j++)
+        {
+        leds(j,k) = CHSV(150, 255, 50);
+          }
+      }
+  for (int m = 3; m < 5; m++)
+    {
+      for (int n = 2; n < 6; n++)
+        {
+          leds(n,m) = CHSV(150, 255, 50);
+          }
+      }
+  leds(3,5) = CHSV(150, 255, 50);
+  leds(4,5) = CHSV(150, 255, 50);
+  leds(3,6) = CHSV(150, 255, 50);
+  leds(4,6) = CHSV(150, 255, 50);
+  leds(3,7) = CHSV(150, 255, 50);
+
+//Drunkard's
+  for (int i = 0; i < 6; i++)
+    {
+      for (int j = 0; j < 6; j++)
+        {
+          leds(i,j) = CHSV(150, 255, 50);
+          }
+      }
+  for (int m = 0; m < 5; m++)
+    {
+      leds(6,m) = CHSV(150, 255, 50);
+      leds(m,6) = CHSV(150, 255, 50);
+      }
+  for (int n = 0; n < 2; n++)
+    {
+      leds(7,n) = CHSV(150, 255, 50);
+      leds(n,7) = CHSV(150, 255, 50);
+      }
+  
+
   
   FastLED.show();
 }
